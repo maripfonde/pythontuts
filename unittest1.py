cc8ffeac5966b5cef3df5b2bd4c54d54905636b4
maripfonde <40838886+maripfonde@users.noreply.github.com>
2018-07-05 00:54:38 +0200

    Add files via upload

---
+++ unittest1.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import unittest
+
+def ExponentCalc(x, y):
+	return x ** y
+
+class ExponetCalcTest(unittest.TestCase):
+	def testSuccess(self):
+		result = ExponentCalc(5,4)
+		self.assertEqual(result, 624)
+
+	def testFailure(self):
+		result = ExponentCalc(3,8)
+		self.assertNotEqual(result, 6560)
+
+
+unittest.main()
+#print(ExponentCalc(5,4))
